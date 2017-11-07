ba6fb0cc832bca9a3036683bf437892145d3b393
sine1019 <502040470@qq.com>
2017-11-07 17:16:28 +0800

    add insert query

---
+++ src/query/insert_query .cpp
@@ -0,0 +1,104 @@
+//
+// Created by admin on 2017/11/2.
+//
+
+#include "insert_query.h"
+#include "../db/db.h"
+#include "../db/db_table.h"
+#include "../formatter.h"
+
+constexpr const char *InsertQuery::qname;
+
+QueryResult::Ptr InsertQuery::execute() {
+	
+    using namespace std;
+    if (!this->operands.empty())
+        return make_unique<ErrorMsgResult>(
+                qname, this->targetTable.c_str(),
+                "Invalid number of operands (? operands)."_f % operands.size()
+        );
+    Database &db = Database::getInstance();
+    Table::SizeType counter = 0;
+    try {
+        auto &table = db[this->targetTable];
+        if (condition.empty()) {
+            counter = table.clear();
+            return make_unique<RecordCountResult>(counter);
+        }
+        addIterationTask<InsertTask>(db, table);
+        return make_unique<RecordCountResult>(counter);
+    } catch (const TableNameNotFound &e) {
+        return make_unique<ErrorMsgResult>(
+                qname, this->targetTable.c_str(),
+                "No such table."s
+        );
+    } catch (const IllFormedQueryCondition &e) {
+        return std::make_unique<ErrorMsgResult>(
+                qname, this->targetTable.c_str(),
+                e.what()
+        );
+    } catch (const invalid_argument &e) {
+        // Cannot convert operand to string
+        return std::make_unique<ErrorMsgResult>(
+                qname, this->targetTable.c_str(),
+                "Unknown error '?'"_f % e.what()
+        );
+    } catch (const exception &e) {
+        return std::make_unique<ErrorMsgResult>(
+                qname, this->targetTable.c_str(),
+                "Unkonwn error '?'."_f % e.what()
+        );
+    }
+}
+
+std::string InsertQuery::toString() {
+    return "QUERY = INSERT " + this->targetTable + "\"";
+}
+
+QueryResult::Ptr InsertQuery::combine() {
+    using namespace std;
+    if (taskComplete < tasks.size()) {
+        return make_unique<ErrorMsgResult>(
+                qname, this->targetTable.c_str(),
+                "Not completed yet."s
+        );
+    }
+    Database &db = Database::getInstance();
+    auto &table = db[this->targetTable];
+    Table::SizeType counter = 0;
+    for (auto &task:tasks) {
+        counter += task->getCounter();
+    }
+    return make_unique<RecordCountResult>(counter);
+}
+
+void InsertTask::execute() {
+    try {
+
+		Table::KeyType key = this->getQuery()->getOperands().front();
+		std::vector<Table::ValueType> data;
+		auto it = ++this->getQuery()->getOperands().begin();
+		for (; it != this->getQuery()->getOperands().end();++it)
+		{
+			data.push_back(strtol(it->c_str(), NULL, 10));
+		}
+		table.insertByIndex(key, data);
+        for (auto it = begin; it != end; ++it) {
+            if (query->evalCondition(query->getCondition(), *it)) {
+				
+                counter++;
+            } else {
+                table.move(it);
+            }
+        }
+        Task::execute();
+    } catch (const IllFormedQueryCondition &e) {
+        return;
+        // @TODO manage query exceptions later
+        /*return std::make_unique<ErrorMsgResult>(
+                query->qname, table.name(),
+                e.what()
+        );*/
+    }
+}
+
